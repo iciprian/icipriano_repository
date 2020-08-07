8e3e8ba38a2a7964d57c64868c5d9dd64f80503b
italo cipriano <icipriano@gmail.com>
2020-08-07 16:21:23 -0400

    new paste_two version

--- codes_paste/paste_two.py
+++ codes_paste/paste_two.py
@@ -1,5 +1,3 @@
 print("paste: second way to do it ")
-<<<<<<< HEAD
 print("colaborador 2 escribio esta linea de codigo y quiero saber si cuando la meta quedara la escoba")
-=======
->>>>>>> 629ae78075fa1b6709a5a66fda650974e177f365
+print("y en efecto quedo la escoba...")
