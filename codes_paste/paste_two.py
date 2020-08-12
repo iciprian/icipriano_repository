04cee81cc077dafcdbca72ed00f55f5ef8b952e1
italo cipriano <icipriano@gmail.com>
2020-08-11 20:35:38 -0400

    I am updating this with empty

--- codes_paste/paste_two.py
+++
@@ -1,3 +0,0 @@
-print("paste: second way to do it ")
-print("colaborador 2 escribio esta linea de codigo y quiero saber si cuando la meta quedara la escoba")
-print("y en efecto quedo la escoba...")
